825ce5a7441af507491898651def29b3420edc83
satoshi <akatsukisatoshi@gmail.com>
2021-01-23 23:05:39 +0900

    <BookList> のテストの styled-components 対応

--- src/Timeline.test.tsx
+++ src/Timeline.test.tsx
@@ -3,21 +3,22 @@ jest.mock('./api')
 import React from 'react'
 import { act } from 'react-dom/test-utils'
 import { mount } from 'enzyme'
-import TimeLine from './TimeLine'
+import Timeline from './Timeline'
+import BookList from './BookList'
 
 describe('<Timeline>', () => {
 
   test('should return <h2> element', async () => {
     await act(async () => {
-      const wrapper = mount(<TimeLine />)
+      const wrapper = mount(<Timeline />)
       expect(wrapper.find('.page__title').text()).toBe('タイムライン')
     })
   })
 
   test('should return <ul> element', async () => {
     await act(async () => {
-      const wrapper = mount(<TimeLine />)
-      expect(wrapper.find('ul').is('.book-list')).toBe(true)
+      const wrapper = mount(<Timeline />)
+      expect(wrapper.find(BookList).length).toBe(1)
     })
   })
 })
