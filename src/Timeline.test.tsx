0a5febfad60bd4755431a89d9308670553bbd6a5
satoshi <akatsukisatoshi@gmail.com>
2021-01-23 22:48:18 +0900

    ページタイトルの表示を確認するテスト

---
+++ src/Timeline.test.tsx
@@ -0,0 +1,16 @@
+jest.mock('./api')
+
+import React from 'react'
+import { act } from 'react-dom/test-utils'
+import { mount } from 'enzyme'
+import TimeLine from './TimeLine'
+
+describe('<Timeline>', () => {
+
+  test('should return <h2> element', async () => {
+    await act(async () => {
+      const wrapper = mount(<TimeLine />)
+      expect(wrapper.find('.page__title').text()).toBe('タイムライン')
+    })
+  })
+})
